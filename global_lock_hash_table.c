444d71f0c4e1857fa6c5e0870c74d9154f36641a
Mpampis Stylianopoulos <mpastyl12@gmail.com>
2014-07-07 13:34:41 +0300

    Added policy and resize

--- global_lock_hash_table.c
+++ global_lock_hash_table.c
@@ -4,13 +4,10 @@
 // node of a list (bucket)
 struct node_t{
     int value;
+    int hash_code;
     struct node_t * next;
 };
-/*
-struct SimpleList{
-    struct node_t * Head;
-}
-*/
+
 
 struct HashSet{
     //int length;
@@ -37,6 +34,8 @@ void unlock_set(struct HashSet * H){
 
 
 
+
+
 //search value in bucket;
 int list_search(struct node_t * Head,int val){
     
@@ -53,7 +52,7 @@ int list_search(struct node_t * Head,int val){
 
 //add value in bucket;
 //NOTE: duplicate values are allowed...
-void list_add(struct HashSet * H, int key,int val){
+void list_add(struct HashSet * H, int key,int val,int hash_code){
     
     struct node_t * curr;
     struct node_t * next;
@@ -72,6 +71,7 @@ void list_add(struct HashSet * H, int key,int val){
     curr->next=node;
     */
     node->value=val;
+    node->hash_code=hash_code;
     if(H->table[key]==NULL) node->next=NULL;
     else node->next=H->table[key];
     H->table[key]=node;
@@ -105,6 +105,9 @@ int list_delete(struct HashSet *H,int key,int val){
 }
 
 
+
+
+
 void initialize(struct HashSet * H, int capacity){
     
     int i;
@@ -117,35 +120,90 @@ void initialize(struct HashSet * H, int capacity){
 }
 
 
-int contains(struct HashSet *H,int key, int val){
+int policy(struct HashSet *H){
+    return ((H->setSize/H->capacity) >4);
+}
+
+void resize(struct HashSet *);
+
+int contains(struct HashSet *H,int hash_code, int val){
     
     lock_set(H);
-    int bucket_index = key % H->capacity;
+    int bucket_index = hash_code % H->capacity;
     int res=list_search(H->table[bucket_index],val);
     unlock_set(H);
     return res;
 }
 
-
-void add(struct HashSet *H,int key, int val){
+//reentrant ==1 means we must not lock( we are calling from resize so we have already locked the data structure)
+void add(struct HashSet *H,int hash_code, int val, int reentrant){
     
-    lock_set(H);
-    int bucket_index = key % H->capacity;
-    list_add(H,bucket_index,val);
+    if(!reentrant) lock_set(H);
+    int bucket_index = hash_code % H->capacity;
+    list_add(H,bucket_index,val,hash_code);
     H->setSize++;
-    unlock_set(H);
+    if(!reentrant) unlock_set(H);
+    if (policy(H)) resize(H);
 }
 
-int delete(struct HashSet *H,int key, int val){
+int delete(struct HashSet *H,int hash_code, int val){
     
     lock_set(H);
-    int bucket_index =  key % H->capacity;
+    int bucket_index =  hash_code % H->capacity;
     int res=list_delete(H,bucket_index,val);
     H->setSize--;
     unlock_set(H);
     return res;
 }
 
+
+void resize(struct HashSet *H){
+    
+    int i;
+    struct node_t * curr;
+    int old_capacity = H->capacity;
+    lock_set(H);
+    if(old_capacity!=H->capacity){
+        unlock_set(H);
+        return; //somebody beat us to it
+    }
+    int new_capacity =  old_capacity * 2;
+    H->capacity =  new_capacity;
+
+    struct node_t ** old_table = H->table;
+    H->setSize=0;
+    H->table = (struct node_t **)malloc(sizeof(struct node_t *)*new_capacity);
+    for(i=0;i<new_capacity;i++){
+        H->table[i]=NULL;
+    }
+    for(i=0;i<old_capacity;i++){
+        
+        curr=old_table[i];
+        while(curr){
+                int val = curr->value;
+                int hash_code = curr->hash_code;
+                //int bucket_index= hash_code % new_capacity;
+                add(H,hash_code,val,1);
+                curr=curr->next;
+        }
+    }
+    free(old_table);
+    unlock_set(H);
+}
+
+void print_set(struct HashSet * H){
+    
+    int i;
+    for(i=0;i<H->capacity;i++){
+        
+        struct node_t * curr=H->table[i];
+        while(curr){
+            printf("(%d) ",curr->value);
+            curr=curr->next;
+        }
+        printf("--\n");
+    }
+}
 
 void main(int argc,char * argv[]){
 
@@ -157,7 +215,7 @@ void main(int argc,char * argv[]){
     printf("res = %d \n",res);
     */
 
-    add(H,4,10);
+    /*add(H,4,10);
     add(H,8,9);
     add(H,8,7);
     add(H,4,5);
@@ -165,7 +223,9 @@ void main(int argc,char * argv[]){
     add(H,0,1);
     add(H,9,12);
     add(H,4,9);
-    printf("delete res %d \n",delete(H,3,3));
+    */
+
+    /*printf("delete res %d \n",delete(H,3,3));
     printf("delete res %d \n",delete(H,3,2));
     printf("delete res %d \n",delete(H,8,7));
     printf("delete res %d \n",delete(H,2,3));
@@ -174,6 +234,17 @@ void main(int argc,char * argv[]){
     printf(" res %d \n",contains(H,3,2));//n
     printf(" res %d \n",contains(H,8,9));//y
     printf(" res %d \n",contains(H,2,3));//n
+    */
+    srand(time(NULL));
+    int i,j;
+    #pragma omp parallel for num_threads(5) shared(H) private(i,j)
+    for(j=0;j<5;j++){
+        for(i=0;i<10;i++){
+            add(H,i+j*10,i+j*10,0);
+            //add(H,rand(),i,0);
+        }
+    }
+    print_set(H);
     return;
 
     
